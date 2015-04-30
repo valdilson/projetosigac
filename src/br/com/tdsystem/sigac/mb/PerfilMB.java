3d76227946efc410ae89199f4fb66e46186b8756
Thiago Oliveira <thiago.krathos@gmail.com>
2015-04-30 11:53:30 -0300

    Criacao Perfil, DAO, Modelo, MB

---
+++ src/br/com/tdsystem/sigac/mb/PerfilMB.java
@@ -0,0 +1,114 @@
+package br.com.tdsystem.sigac.mb;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import br.com.tdsystem.sigac.dao.PerfilDAO;
+import br.com.tdsystem.sigac.modelo.Perfil;
+import br.com.tdsystem.sigac.util.FacesUtil;
+
+@ManagedBean
+@ViewScoped
+public class PerfilMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<Perfil> listaperfis;
+	private Perfil perfil = null;
+	private PerfilDAO perfilDAO =  null;
+	
+	
+	
+	public List<Perfil> getListaperfis() {
+		return listaperfis;
+	}
+
+	public void setListaperfis(List<Perfil> listaperfis) {
+		this.listaperfis = listaperfis;
+	}
+
+	public Perfil getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(Perfil perfil) {
+		this.perfil = perfil;
+	}
+
+	public PerfilDAO getPerfilDAO() {
+		return perfilDAO;
+	}
+
+	public void setPerfilDAO(PerfilDAO perfilDAO) {
+		this.perfilDAO = perfilDAO;
+	}
+
+	public void salvar() {
+
+		try {
+			perfilDAO = new PerfilDAO();
+			perfilDAO.salvar(perfil);
+			FacesUtil.exibirMensagemSucesso("Cadastro feito com Sucesso!");
+
+		} catch (RuntimeException e) {
+			FacesUtil.exibirMensagemErro("Erro ao gravar registro!"
+					+ e.getMessage());
+		}
+	}
+
+	public void excluir() {
+
+		try {
+			perfilDAO = new PerfilDAO();
+			perfilDAO.exluir(perfil);
+			FacesUtil.exibirMensagemSucesso("Exclusão feita com Sucesso!");
+
+		} catch (RuntimeException e) {
+			FacesUtil.exibirMensagemErro("Erro ao excluir registro!"
+					+ e.getMessage());
+		}
+	}
+
+	public void editar() {
+
+		try {
+			perfilDAO = new PerfilDAO();
+			perfilDAO.editar(perfil);
+			FacesUtil.exibirMensagemSucesso("Edição feita com Sucesso!");
+
+		} catch (RuntimeException e) {
+			FacesUtil.exibirMensagemErro("Erro ao editar registro!"
+					+ e.getMessage());
+		}
+	}
+
+	public void listarperfils() {
+
+		try {
+			perfilDAO = new PerfilDAO();
+			listaperfis = perfilDAO.listaPerfil();
+
+		} catch (RuntimeException e) {
+			FacesUtil.exibirMensagemErro("Não retornou registro!"
+					+ e.getMessage());
+		}
+	}
+
+	public void pesquisaCodigo() {
+
+		try {
+			String valorRecebido = FacesUtil.getParametro("codigoperfil");
+			if (valorRecebido != null) {
+				perfilDAO = new PerfilDAO();
+				Long codigo = Long.parseLong(valorRecebido);
+				perfil = perfilDAO.pesquisaCodigo(codigo);
+			}
+
+		} catch (RuntimeException e) {
+			FacesUtil.exibirMensagemErro("Não retornou registro!"
+					+ e.getMessage());
+		}
+	}
+
+}
---
+++ src/br/com/tdsystem/sigac/modelo/Perfil.java
@@ -0,0 +1,65 @@
+package br.com.tdsystem.sigac.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table
+@NamedQueries({
+	@NamedQuery(name="Perfil.lista", query="Select perfil from Perfil perfil"),
+	@NamedQuery(name="Perfil.codigo", query="Select perfil from Perfil perfil where perfil.codigo = :codigo"),
+	@NamedQuery(name="Perfil.nome", query="Select perfil from Perfil perfil where perfil.nome = :nome"),
+	@NamedQuery(name="Perfil.status", query="Select perfil from Perfil perfil where perfil.status = :status")
+})
+public class Perfil implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name="codigo")
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long codigo;
+	
+	@Column(name="nome")
+	@NotEmpty(message="Campo nome obrigatório")
+	private String nome;
+	
+	@Column(name="status")
+	@NotEmpty(message="Campo status obrigatório")
+	private Boolean status;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	
+
+}
